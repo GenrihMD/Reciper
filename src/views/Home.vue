a30d640f7600976fbccb9e4b7271b2d105bb7fc3
laamin <genrih.md@gmail.com>
2020-11-04 13:05:21 +0300

    Style code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,12 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+<template lang="pug">
+.home
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<script lang="ts">
+import { HelloWorld } from "@/components/HelloWorld.vue";
+import { Vue, Component } from "vue-property-decorator";
 
-export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
-};
+@Component
+export class Home extends Vue {}
+export default Home
 </script>
